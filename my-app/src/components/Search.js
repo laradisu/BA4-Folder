4f9d6c82d3a5c31bc8c6d42f662a56cb182e1651
laradisu <32111302+laradisu@users.noreply.github.com>
2019-11-03 21:16:12 -0500

    search border added

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -3,15 +3,20 @@ import React from 'react';
 class Search extends React.Component {
 	filterUpdate() {
 		//Here you will need to update the value of the filter with the value from the textbox
-	}
+	 }
 	render() {
 		//You will need to save the value from the textbox and update it as it changes
 		//You will need the onChange value for the input tag to capture the textbox value
 		
 		return (
-			<form>
-				<input type="text" placeholder="Type to Filter" />
-			</form>
+			<header>
+				<form>
+					<input 
+						type="text" 
+						placeholder="Type to Filter" 
+					/>
+				</form>
+			</header>	
 		);
 	}
 }

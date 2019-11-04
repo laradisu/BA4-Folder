a5d6f3234835876b29f448133a70f3abb34427dd
laradisu <32111302+laradisu@users.noreply.github.com>
2019-11-03 23:14:30 -0500

    pass callback function instead of string

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Search extends React.Component {
 	filterUpdate() {
 		//Here you will need to update the value of the filter with the value from the textbox
-	 }
+		const val = this.myValue.value
+
+	}
 	render() {
 		//You will need to save the value from the textbox and update it as it changes
 		//You will need the onChange value for the input tag to capture the textbox value
@@ -13,7 +15,9 @@ class Search extends React.Component {
 				<form>
 					<input 
 						type="text" 
-						placeholder="Type to Filter" 
+						ref= { (value) => this.myValue = value}
+						placeholder="Type to Filter"
+						onChange = {this.filterUpdate.bind(this)} 
 					/>
 				</form>
 			</header>	

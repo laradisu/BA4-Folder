5fc0a734a22ec6d29af7f2ae0812121eeee1e204
laradisu <32111302+laradisu@users.noreply.github.com>
2019-11-04 13:03:41 -0500

    filters building names but not codes

--- my-app/src/components/BuildingList.js
+++ my-app/src/components/BuildingList.js
@@ -3,9 +3,14 @@ import React from 'react';
 class BuilingList extends React.Component {
 	render() {
 		//console.log('This is my directory file', this.props.data);
-		const { data } = this.props;
+		const { data, filterText } = this.props;
 
-		const buildingList = data.map(directory => {
+		const buildingList = data
+			.filter(name => {
+				//remove names that do not match current filter text
+				return name.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0
+			})
+			.map(directory => {
 			return (
 				<tr key={directory.id}>
 					<td>{directory.code} </td>

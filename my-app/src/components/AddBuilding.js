13890fdcf620dacbf5c66d2362cbfcafd37a2dc3
laradisu <32111302+laradisu@users.noreply.github.com>
2019-11-05 17:53:57 -0500

    deleted unneeded comments

--- my-app/src/components/AddBuilding.js
+++
@@ -1,8 +0,0 @@
-/*
-addFavourite(id) {
-    const newSet = this.state.favourites.concat(id)
-    this.setState({
-        favourites: newSet
-    })
-}
-*/
--- my-app/src/components/RemoveBuilding.js
+++ my-app/src/components/RemoveBuilding.js
@@ -14,15 +14,3 @@ export default class RemoveBuilding extends Component {
         )
     }
 }
-/*
-deleteFavourite(id){
-    const { favourites } = this.state
-    const newList = [
-        ...favourites.slice(0,id),
-        ...favourites.slice(id + 1)
-    ]
-    this.setState({
-        favourites: newList
-    })
-}
-*/
